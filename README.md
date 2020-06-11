f3a6444bffce51eccac00ec4294064b99dc40f91
Yacine Mahdid <yacine.mahdid@mail.mcgill.ca>
2020-06-11 17:38:39 -0400

    Restructuration of the code to match the milestones architecture

--- README.md
+++ README.md
@@ -5,4 +5,5 @@ This repository is for the study of AEC vs wPLI functional connectivity metric a
 In this repository we have adopted a iterative milestone approach. Meaning that the code was built and improved by trying to hit a clear objecive than make the analysis better. The most up to date analysis is the one with the highest integer prefix, but all previous iteration are present to ensure that all prior experiments are available without going back into the git history.
 
 ### Milestones
-1. OHBM 2020 Poster Presentation
+0. First Attempt at Classification: this is the code used to generate the result for the first draft of the paper sent out for review.
+1. OHBM 2020 Poster Presentation : this analysis was presented at OHBM and was focused on the binarized vs non-binarized graph.
--- milestones/1_ohbm_2020_poster_presentation/generate_features.m
+++ milestones/1_ohbm_2020_poster_presentation/generate_features.m
@@ -8,10 +8,42 @@
 % Data are N*82*82 matrices where N is the number of 10 seconds window and
 % 82 is the number of channels
 
-% Experimental Variables
+%% Experimental Variables
 DATA_PATH = "/media/yacine/My Book/datasets/consciousness/aec_wpli_source_localized_data/";
 FREQUENCY = "alpha"; % we can only use alpha here since ec8 was only calculated for that
 TYPES = ["aec", "pli"];
 EPOCHS = ["ec1","emf5", "ec8"];
 
+%% Generate each participant feature
 files = dir(DATA_PATH);
+for id = 3:length(files)
+    file = files(id);
+    [freq, type, epoch, data] = load_data(file);
+    
+    fprintf("freq = %s, type = %s epoch = %s\n", freq, type, epoch);
+    
+
+
+    
+end
+
+function [freq, type, epoch, data] = load_data(file)
+% Load Data: Will load data from the file and extract metainfo from filename    
+
+    data_path = strcat(file.folder, filesep, file.name);
+    
+    % Get meta-info
+    content  = strsplit(file.name,'_');
+    type = content{1};
+    epoch = content{2};
+    freq = content{3};
+    
+    raw = load(data_path);
+    
+    if strcmp(type, "aec")
+        data = raw.AEC_OUT;
+    else
+        data = raw.PLI_OUT;
+    end
+    
+end
