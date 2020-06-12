1427e6e4cf866dcb12d00481316a7a2823dd622e
Yacine Mahdid <yacine.mahdid@mail.mcgill.ca>
2020-06-11 23:47:28 -0400

    Increase the null-networks to 100

--- milestones/2_ohbm_2020_poster_presentation/generate_features.m
+++ milestones/2_ohbm_2020_poster_presentation/generate_features.m
@@ -36,7 +36,7 @@ EPOCHS = ["ec1", "if5", "emf5", "eml5", "ec8"];
 
 % Graph theory paramters
 num_regions = 82; % Number of source localized regions
-num_null_network = 10; % Number of null network to create 
+num_null_network = 100; % Number of null network to create 
 bin_swaps = 10;  % used to create the null network
 weight_frequency = 0.1; % used to create the null network
 t_level = 0.1; % Threshold level (keep 10%)
