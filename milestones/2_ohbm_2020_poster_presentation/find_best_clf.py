1cffb78d58e2148a119b58034d1e16fecd020b43
Yacine Mahdid <yacine.mahdid@mail.mcgill.ca>
2020-06-13 10:09:36 -0400

    Add in generate bootstrap interval

--- milestones/2_ohbm_2020_poster_presentation/find_best_clf.py
+++ milestones/2_ohbm_2020_poster_presentation/find_best_clf.py
@@ -1,17 +1,9 @@
 import sys
 import pickle
-import pandas as pd
 import numpy as np 
 
 from sklearn.pipeline import Pipeline
-from sklearn.base import BaseEstimator
 from sklearn.model_selection import GridSearchCV
-
-from sklearn.linear_model import LogisticRegression
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.svm import LinearSVC
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 
 from sklearn.preprocessing import StandardScaler
 from sklearn.impute import SimpleImputer
@@ -21,31 +13,7 @@ from sklearn.model_selection import LeaveOneGroupOut
 from ml_tools.classification import classify_loso_model_selection
 import config as cfg
 
-
-def print_summary(accuracies, group, df):
-    """
-    Helper function to print a summary of a classifier performance
-    :param accuracies: a list of the accuracy obtained across fold (participant)
-    :param group: ids of the participants windows
-    :param df: dataframe containing all the data about the participant
-    :return: None
-    """
-
-    p_ids = np.unique(group)
-    print("Accuracies: ")
-    for accuracy, p_id in zip(accuracies, p_ids):
-        print(f"Participant {p_id}: accuracy = {accuracy}")
-
-    print(f"Mean accuracy: {np.mean(accuracies)}")
-
-# Create LOSO Grid Search to search amongst many classifier
-class DummyEstimator(BaseEstimator):
-    """Dummy estimator to allow gridsearch to test many estimator"""
-
-    def fit(self): pass
-    
-    def score(self): pass
-
+from utils import DummyEstimator, print_summary, filter_dataframe, search_space
 
 # Get the argument
 analysis_param = sys.argv[1]
@@ -57,22 +25,7 @@ best_clf_filename = cfg.OUTPUT_DIR + f'best_clf_{analysis_param}.pickle'
 
 print(f"Graph {graph} at ec1 vs {epoch} with feature {feature_group}")
 
-# Read the CSV
-df = pd.read_csv(cfg.DF_FILE_PATH)
-
-# Keep only the Graph of interest
-df = df[df.graph == cfg.GRAPHS.index(graph)]
-
-# Keep only the epoch of interest
-df = df[(df.epoch == cfg.EPOCHS[epoch]) | (df.epoch == cfg.EPOCHS['ec1'])]
-
-# Keep only the features of interest
-df.drop(df.filter(regex=cfg.FILTER_REGEX[feature_group]), axis=1, inplace=True)
-
-# Set the up the feature matrix, the label vector and the group ids
-X = df.drop(['p_id', 'frequency', 'epoch','graph','window'], axis=1).to_numpy()
-y = df.epoch.to_numpy()
-group = df.p_id.to_numpy()
+X,y,group = filter_dataframe(graph, epoch, feature_group)
 
 # Create a pipeline
 pipe = Pipeline([
@@ -80,29 +33,7 @@ pipe = Pipeline([
     ('scaler', StandardScaler()),
     ('clf', DummyEstimator())])  # Placeholder Estimator
 
-# Candidate learning algorithms and their hyperparameters
-search_space = [{'clf': [LogisticRegression()],  
-                 'clf__penalty': ['l2'],
-                 'clf__solver': ['lbfgs'],
-                 'clf__max_iter': [1000],
-                 'clf__C': np.logspace(0, 4, 10)},
-
-                {'clf': [LinearDiscriminantAnalysis()],
-                 'clf__solver': ['svd', 'lsqr']},
-
-                {'clf': [LinearSVC()],
-                 'clf__C': [1, 10, 100, 1000]},
-
-                {'clf': [RandomForestClassifier()],
-                 'clf__n_estimators': np.linspace(200, 2000, 4, dtype=int),
-                 'clf__max_depth': np.linspace(10, 100, 4),
-                 'clf__min_samples_split': [3, 10],
-                 'clf__min_samples_leaf': [1, 4]},
-
-                {'clf': [DecisionTreeClassifier()],  # Actual Estimator
-                 'clf__criterion': ['gini', 'entropy']}]
-
-# We will try to use as many processor as possible for the gridsearch
+# We will use as many processor as possible for the gridsearch
 gs = GridSearchCV(pipe, search_space, cv=LeaveOneGroupOut(), n_jobs=-1)
 
 accuracies, f1s, cms, best_params = classify_loso_model_selection(X, y, group, gs)
@@ -120,4 +51,4 @@ pickle.dump(clf_data, best_clf_file)
 best_clf_file.close()
 
 # Print out the summary in the console
-print_summary(accuracies, group, df)
+print_summary(accuracies, group)
---
+++ milestones/2_ohbm_2020_poster_presentation/generate_bootstrap_distribution.py
@@ -0,0 +1,60 @@
+import pickle
+
+import pandas as pd
+import numpy as np
+
+from sklearn.pipeline import Pipeline
+from sklearn.base import BaseEstimator
+from sklearn.model_selection import GridSearchCV
+
+from sklearn.preprocessing import StandardScaler
+from sklearn.impute import SimpleImputer
+from sklearn.model_selection import LeaveOneGroupOut
+
+import config as cfg
+from ml_tools.classification import bootstrap_interval
+from utils import load_pickle, find_best_model, filter_dataframe
+
+
+# This will be given by the srun in the bash file
+arg = "pli_emf5_func-wei"
+analysis_param = arg
+
+# Get the argument
+#analysis_param = sys.argv[1]
+
+bootstrap_filename = cfg.OUTPUT_DIR + f"bootstrap_{analysis_param}.pickle"
+best_clf_filename = cfg.OUTPUT_DIR + f'best_clf_{analysis_param}.pickle'
+
+# Parse the parameters
+(graph, epoch, feature_group) = analysis_param.split("_")
+
+print(f"Graph {graph} at ec1 vs {epoch} with feature {feature_group}")
+
+X, y, group = filter_dataframe(graph, epoch, feature_group)
+
+best_clf_data = load_pickle(best_clf_filename)
+clf = find_best_model(best_clf_data['best_params'])
+pipe = Pipeline([
+    ('imputer', SimpleImputer(missing_values=np.nan, strategy='mean')),
+    ('scaler', StandardScaler()),
+    ('SVM', clf)])
+
+# Training and bootstrap interval generation
+acc_distribution, acc_interval = bootstrap_interval(X, y, group, pipe, num_resample=10, p_value=0.05)
+
+# Save the data to disk
+bootstrap_file = open(bootstrap_filename, 'ab')
+bootstrap_data = {
+    'distribution': acc_distribution,
+    'interval': acc_interval
+}
+pickle.dump(bootstrap_data, bootstrap_file)
+bootstrap_file.close()
+
+# Print out some high level sumarry
+print("F1 Distribution:")
+print(acc_distribution)
+print(f"Mean: {np.mean(acc_distribution)} and std: {np.std(acc_distribution)}")
+print("Bootstrap Interval")
+print(acc_interval)
