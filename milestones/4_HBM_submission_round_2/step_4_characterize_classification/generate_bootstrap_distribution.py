6b808147ca15e549c8fffa0e149b82ba7ff4ab7a
CharlotteMaschke <55599742+CharlotteMaschke@users.noreply.github.com>
2020-11-10 23:53:30 -0500

    adapted bootstrap

--- milestones/4_HBM_submission_round_2/step_4_characterize_classification/generate_bootstrap_distribution.py
+++ milestones/4_HBM_submission_round_2/step_4_characterize_classification/generate_bootstrap_distribution.py
@@ -25,42 +25,59 @@ from commons import load_pickle, find_best_model, filter_dataframe
 
 # This will be given by the srun in the bash file
 # Get the argument
-analysis_param = sys.argv[1]
-
-bootstrap_filename = commons.OUTPUT_DIR + f"bootstrap_{analysis_param}.pickle"
-best_clf_filename = commons.OUTPUT_DIR + f'best_clf_{analysis_param}.pickle'
 
 # Parse the parameters
-(graph, epoch, feature_group) = analysis_param.split("_")
+EPOCHS = {"ind","emf5","eml5","ec8"} # to compare against baseline
+GRAPHS = ["aec", "pli", "both"]
 
-print(f"Bootstrap: Graph {graph} at ec1 vs {epoch} with feature {feature_group}")
+for graph in GRAPHS:
+    for epoch in EPOCHS:
 
-X, y, group = filter_dataframe(graph, epoch, feature_group)
+        bootstrap_filename = commons.OUTPUT_DIR + f"bootstrap_{graph}_ec1_vs_{epoch}.pickle"
+        best_clf_filename = final_acc_filename = commons.OUTPUT_DIR + f"FINAL_MODEL_{graph}_ec1_vs_{epoch}_raw.pickle"
 
-best_clf_data = load_pickle(best_clf_filename)
-clf = find_best_model(best_clf_data['best_params'])
+        print(f"Bootstrap: Graph {graph} at ec1 vs {epoch}")
 
+        if graph != "both":
+            print(f"MODE {graph}")
+            print(f"FINAL SVC Model at Graph {graph} at ec1 vs {epoch}")
+            X, y, group = filter_dataframe(graph, epoch)
 
-pipe = Pipeline([
-    ('imputer', SimpleImputer(missing_values=np.nan, strategy='mean')),
-    ('scaler', StandardScaler()),
-    ('CLF', clf)]) # ! THIS IS NOT NECESSARY THE SVM; IT ONLY IS THE BEST CLASSIFIER
+        if graph == "both":
+            print(f"MODE {graph}")
+            print(f"FINAL SVC Model at Graph {graph} at ec1 vs {epoch}")
+            X_pli, y_pli, group_pli = filter_dataframe('pli', epoch)
+            X_aec, y_aec, group_aec = filter_dataframe('aec', epoch)
+            X = np.hstack((X_aec, X_pli))
+            if np.array_equal(y_aec, y_pli):
+                print("Y-values equal")
+                y = y_aec
+            if np.array_equal(group_aec, group_pli):
+                print("group-values equal")
+                group = group_aec
 
-# Training and bootstrap interval generation
-acc_distribution, acc_interval = bootstrap_interval(X, y, group, pipe, num_resample=1000, p_value=0.05)
+        clf = commons.best_model
 
-# Save the data to disk
-bootstrap_file = open(bootstrap_filename, 'ab')
-bootstrap_data = {
-    'distribution': acc_distribution,
-    'interval': acc_interval
-}
-pickle.dump(bootstrap_data, bootstrap_file)
-bootstrap_file.close()
+        pipe = Pipeline([
+            ('imputer', SimpleImputer(missing_values=np.nan, strategy='mean')),
+            ('scaler', StandardScaler()),
+            ('CLF', clf)])
 
-# Print out some high level summary
-print("Accuracy Distribution:")
-print(acc_distribution)
-print(f"Mean: {np.mean(acc_distribution)} and std: {np.std(acc_distribution)}")
-print("Bootstrap Interval")
-print(acc_interval)
+        # Training and bootstrap interval generation
+        acc_distribution, acc_interval = bootstrap_interval(X, y, group, pipe, num_resample=1000, p_value=0.05)
+
+        # Save the data to disk
+        bootstrap_file = open(bootstrap_filename, 'ab')
+        bootstrap_data = {
+            'distribution': acc_distribution,
+            'interval': acc_interval
+        }
+        pickle.dump(bootstrap_data, bootstrap_file)
+        bootstrap_file.close()
+
+        # Print out some high level summary
+        print("Accuracy Distribution:")
+        print(acc_distribution)
+        print(f"Mean: {np.mean(acc_distribution)} and std: {np.std(acc_distribution)}")
+        print("Bootstrap Interval")
+        print(acc_interval)
