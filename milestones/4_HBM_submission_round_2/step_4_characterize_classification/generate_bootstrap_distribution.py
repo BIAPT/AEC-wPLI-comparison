7a302cba8cc3a2a1e146110284d81d0b895e56dc
CharlotteMaschke <55599742+CharlotteMaschke@users.noreply.github.com>
2020-11-16 15:35:25 -0500

    adapt bootstrap and permutation to run on both

--- milestones/4_HBM_submission_round_2/step_4_characterize_classification/generate_bootstrap_distribution.py
+++ milestones/4_HBM_submission_round_2/step_4_characterize_classification/generate_bootstrap_distribution.py
@@ -15,9 +15,9 @@ from sklearn.impute import SimpleImputer
 
 # Add the directory containing your module to the Python path (wants absolute paths)
 # Here we add to the path everything in the top level
-# Since the code will be called from generate_jobs.bash the path that needs to be added is the 
+# Since the code will be called from generate_jobs.bash the path that needs to be added is the
 # one from the bash script (top level)
-scriptpath = "." 
+scriptpath = "."
 sys.path.append(os.path.abspath(scriptpath))
 
 # Common import across analysis
@@ -31,73 +31,66 @@ from commons import load_pickle, find_best_model, filter_dataframe
 # Parse the parameters
 EPOCHS = {"ind","emf5","eml5","ec8"} # to compare against baseline
 GRAPHS = ["aec", "pli", "both"]
+Steps = ['01', '10']
 
-bootstrap_filename = commons.OUTPUT_DIR + f"bootstrap_Final_model_SUMMARY.csv"
-boot_data=pd.DataFrame(np.zeros((len(EPOCHS)*len(GRAPHS),6)))
-names=['epoch','graph','Acc_Dist Mean', 'Acc_Dist Std', 'acc_interval_low', 'acc_interval_high']
-boot_data.columns=names
-c=0
+for s in Steps:
+    bootstrap_filename = commons.OUTPUT_DIR + f"bootstrap_Final_model_step_{s}_SUMMARY.csv"
+    boot_data = pd.DataFrame(np.zeros((len(EPOCHS) * len(GRAPHS), 6)))
+    names = ['epoch', 'graph', 'Acc_Dist Mean', 'Acc_Dist Std', 'acc_interval_low', 'acc_interval_high']
+    boot_data.columns = names
+    c = 0
 
-for graph in GRAPHS:
-    for epoch in EPOCHS:
+    for graph in GRAPHS:
+        for epoch in EPOCHS:
+            best_clf_filename = final_acc_filename = commons.OUTPUT_DIR + f"final_models/FINAL_MODEL_{graph}_ec1_vs_{epoch}_step_{s}.pickle"
 
-        best_clf_filename = final_acc_filename = commons.OUTPUT_DIR + f"FINAL_MODEL_{graph}_ec1_vs_{epoch}_raw.pickle"
+            print(f"Bootstrap: Graph {graph} at ec1 vs {epoch}")
 
-        print(f"Bootstrap: Graph {graph} at ec1 vs {epoch}")
+            if graph != "both":
+                print(f"MODE {graph}")
+                print(f"FINAL Model Graph {graph} at ec1 vs {epoch}")
+                X, y, group = filter_dataframe(graph, epoch, s)
 
-        if graph != "both":
-            print(f"MODE {graph}")
-            print(f"FINAL SVC Model at Graph {graph} at ec1 vs {epoch}")
-            X, y, group = filter_dataframe(graph, epoch)
+            if graph == "both":
+                print(f"MODE {graph}")
+                print(f"FINAL Model Graph {graph} at ec1 vs {epoch}")
+                X_pli, y_pli, group_pli = filter_dataframe('pli', epoch, s)
+                X_aec, y_aec, group_aec = filter_dataframe('aec', epoch, s)
+                X = np.hstack((X_aec, X_pli))
+                if np.array_equal(y_aec, y_pli):
+                    print("Y-values equal")
+                    y = y_aec
+                if np.array_equal(group_aec, group_pli):
+                    print("group-values equal")
+                    group = group_aec
 
-        if graph == "both":
-            print(f"MODE {graph}")
-            print(f"FINAL SVC Model at Graph {graph} at ec1 vs {epoch}")
-            X_pli, y_pli, group_pli = filter_dataframe('pli', epoch)
-            X_aec, y_aec, group_aec = filter_dataframe('aec', epoch)
-            X = np.hstack((X_aec, X_pli))
-            if np.array_equal(y_aec, y_pli):
-                print("Y-values equal")
-                y = y_aec
-            if np.array_equal(group_aec, group_pli):
-                print("group-values equal")
-                group = group_aec
+            clf = commons.best_model
 
-        clf = commons.best_model
+            pipe = Pipeline([
+                ('imputer', SimpleImputer(missing_values=np.nan, strategy='mean')),
+                ('scaler', StandardScaler()),
+                ('CLF', clf)])
 
-        pipe = Pipeline([
-            ('imputer', SimpleImputer(missing_values=np.nan, strategy='mean')),
-            ('scaler', StandardScaler()),
-            ('CLF', clf)])
+            # Training and bootstrap interval generation
+            acc_distribution, acc_interval = bootstrap_interval(X, y, group, pipe, num_resample=1000, p_value=0.05)
 
-        # Training and bootstrap interval generation
-        acc_distribution, acc_interval = bootstrap_interval(X, y, group, pipe, num_resample=1000, p_value=0.05)
+            # Print out some high level summary
+            print("Accuracy Distribution:")
+            print(acc_distribution)
+            print(f"Mean: {np.mean(acc_distribution)} and std: {np.std(acc_distribution)}")
+            print("Bootstrap Interval")
+            print(acc_interval)
 
-        # Save the data to disk
-        #bootstrap_file = open(bootstrap_filename, 'ab')
-        #bootstrap_data = {
-        #    'distribution': acc_distribution,
-        #    'interval': acc_interval
-        #}
-        #pickle.dump(bootstrap_data, bootstrap_file)
-        #bootstrap_file.close()
+            boot_data.loc[c, 'epoch'] = epoch
+            boot_data.loc[c, 'graph'] = graph
+            boot_data.loc[c, 'Acc_Dist Mean'] = np.mean(acc_distribution)
+            boot_data.loc[c, 'Acc_Dist Std'] = np.std(acc_distribution)
+            boot_data.loc[c, 'acc_interval_low'] = acc_interval[0]
+            boot_data.loc[c, 'acc_interval_high'] = acc_interval[1]
 
-        # Print out some high level summary
-        print("Accuracy Distribution:")
-        print(acc_distribution)
-        print(f"Mean: {np.mean(acc_distribution)} and std: {np.std(acc_distribution)}")
-        print("Bootstrap Interval")
-        print(acc_interval)
+            c += 1
 
-        boot_data.loc[c, 'epoch'] = epoch
-        boot_data.loc[c, 'graph'] = graph
-        boot_data.loc[c, 'Acc_Dist Mean'] = np.mean(acc_distribution)
-        boot_data.loc[c, 'Acc_Dist Std'] = np.std(acc_distribution)
-        boot_data.loc[c, 'acc_interval_low'] = acc_interval[0]
-        boot_data.loc[c, 'acc_interval_high'] = acc_interval[1]
+    boot_data.to_csv(bootstrap_filename, index=False, header= True, sep=',')
 
-        c += 1
-
-boot_data.to_csv(bootstrap_filename, index=False, header= True, sep=',')
 print('finished')
 
--- milestones/4_HBM_submission_round_2/step_4_characterize_classification/generate_permutations_tests.py
+++ milestones/4_HBM_submission_round_2/step_4_characterize_classification/generate_permutations_tests.py
@@ -28,69 +28,65 @@ from commons import load_pickle, find_best_model, filter_dataframe
 # Parse the parameters
 EPOCHS = {"ind","emf5","eml5","ec8"} # to compare against baseline
 GRAPHS = ["aec", "pli", "both"]
+Steps = ['01', '10']
 
-permutation_filename = commons.OUTPUT_DIR + f"permutation_Final_model_SUMMARY.csv"
-perm_data=pd.DataFrame(np.zeros((len(EPOCHS)*len(GRAPHS),5)))
-names=['epoch','graph','Random Mean', 'Accuracy', 'p-value']
-perm_data.columns=names
-c=0
+for s in Steps:
+    permutation_filename = commons.OUTPUT_DIR + f"permutation_Final_model_step_{s}_SUMMARY.csv"
+    perm_data = pd.DataFrame(np.zeros((len(EPOCHS) * len(GRAPHS), 5)))
+    names=['epoch','graph','Random Mean', 'Accuracy', 'p-value']
+    perm_data.columns=names
+    c=0
 
+    for graph in GRAPHS:
+        for epoch in EPOCHS:
 
-for graph in GRAPHS:
-    for epoch in EPOCHS:
+            best_clf_filename = final_acc_filename = commons.OUTPUT_DIR + f"final_models/FINAL_MODEL_{graph}_ec1_vs_{epoch}_step_{s}.pickle"
 
-        best_clf_filename = final_acc_filename = commons.OUTPUT_DIR + f"FINAL_MODEL_{graph}_ec1_vs_{epoch}_raw.pickle"
+            print(f"Permutation: Graph {graph} at ec1 vs {epoch}")
 
-        print(f"Permutation: Graph {graph} at ec1 vs {epoch}")
+            if graph != "both":
+                print(f"MODE {graph}")
+                print(f"FINAL Model Graph {graph} at ec1 vs {epoch}")
+                X, y, group = filter_dataframe(graph, epoch, s)
 
-        if graph != "both":
-            print(f"MODE {graph}")
-            print(f"FINAL SVC Model at Graph {graph} at ec1 vs {epoch}")
-            X, y, group = filter_dataframe(graph, epoch)
+            if graph == "both":
+                print(f"MODE {graph}")
+                print(f"FINAL Model Graph {graph} at ec1 vs {epoch}")
+                X_pli, y_pli, group_pli = filter_dataframe('pli', epoch, s)
+                X_aec, y_aec, group_aec = filter_dataframe('aec', epoch, s)
+                X = np.hstack((X_aec, X_pli))
+                if np.array_equal(y_aec, y_pli):
+                    print("Y-values equal")
+                    y = y_aec
+                if np.array_equal(group_aec, group_pli):
+                    print("group-values equal")
+                    group = group_aec
 
-        if graph == "both":
-            print(f"MODE {graph}")
-            print(f"FINAL SVC Model at Graph {graph} at ec1 vs {epoch}")
-            X_pli, y_pli, group_pli = filter_dataframe('pli', epoch)
-            X_aec, y_aec, group_aec = filter_dataframe('aec', epoch)
-            X = np.hstack((X_aec, X_pli))
-            if np.array_equal(y_aec, y_pli):
-                print("Y-values equal")
-                y = y_aec
-            if np.array_equal(group_aec, group_pli):
-                print("group-values equal")
-                group = group_aec
+            clf = commons.best_model
 
-        clf = commons.best_model
+            pipe = Pipeline([
+                ('imputer', SimpleImputer(missing_values=np.nan, strategy='mean')),
+                ('scaler', StandardScaler()),
+                ('CLF', clf)])
 
-        pipe = Pipeline([
-            ('imputer', SimpleImputer(missing_values=np.nan, strategy='mean')),
-            ('scaler', StandardScaler()),
-            ('CLF', clf)])
+            # Training and bootstrap interval generation
+            acc, perms, p_value = permutation_test(X, y, group, pipe, num_permutation=1000)
 
-        # Training and bootstrap interval generation
-        acc, perms, p_value = permutation_test(X, y, group, pipe, num_permutation=1000)
+            # Print out some high level summary
+            print("Random:")
+            print(np.mean(perms))
+            print("Actual Improvement")
+            print(acc)
+            print("P Value:")
+            print(p_value)
 
-        # Print out some high level summary
-        print("Random:")
-        print(np.mean(perms))
-        print("Actual Improvement")
-        print(acc)
-        print("P Value:")
-        print(p_value)
+            perm_data.loc[c, 'epoch'] = epoch
+            perm_data.loc[c, 'graph'] = graph
+            perm_data.loc[c, 'Random Mean'] = np.mean(perms)
+            perm_data.loc[c, 'Accuracy'] = acc
+            perm_data.loc[c, 'p-value'] = p_value
 
-        perm_data.loc[c, 'epoch'] = epoch
-        perm_data.loc[c, 'graph'] = graph
-        perm_data.loc[c, 'Random Mean'] = np.mean(perms)
-        perm_data.loc[c, 'Accuracy'] = acc
-        perm_data.loc[c, 'p-value'] = p_value
-
-        c += 1
-
-        # Save the data to disk
-        #perms_file = open(permutation_filename, 'ab')
-        #pickle.dump([perms, acc, p_value], perms_file)
-        #perms_file.close()
-
-perm_data.to_csv(permutation_filename, index=False, header= True, sep=',')
+            c += 1
+    
+    perm_data.to_csv(permutation_filename, index=False, header= True, sep=',')
 print('finished')
